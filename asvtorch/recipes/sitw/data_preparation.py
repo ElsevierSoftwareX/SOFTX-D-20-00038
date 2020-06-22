b0367c78c766318e044a7e5c89ba111721371592
Ville Vestman <ville.vestman@gmail.com>
2020-06-22 17:00:33 +0300

    SITW recipe added

---
+++ asvtorch/recipes/sitw/data_preparation.py
@@ -0,0 +1,128 @@
+from typing import List, Optional
+import os
+import subprocess
+
+from asvtorch.src.misc.fileutils import ensure_exists
+from asvtorch.src.settings.settings import Settings
+import asvtorch.src.misc.fileutils as fileutils
+
+
+def prepare_datasets(datasets: Optional[List[str]]):
+    dataset2func = {
+        'sitw': prepare_sitw,
+        'voxceleb1': prepare_voxceleb1,
+        'voxceleb2': prepare_voxceleb2
+    }
+    if datasets is None:  # If no list is given, prepare all
+        datasets = dataset2func.keys()
+    for dataset in datasets:
+        dataset2func[dataset]()
+        print(sep='\n\n')
+
+
+def prepare_sitw():
+    print('Preparing SITW...')
+    sitw_folder = Settings().paths.datasets['sitw']
+    output_folder = fileutils.get_list_folder('sitw')
+    ensure_exists(output_folder)
+    enroll_file = os.path.join(sitw_folder, 'eval', 'lists', 'enroll-core.lst')
+    core_trials_file = os.path.join(sitw_folder, 'eval', 'keys', 'core-core.lst')
+    multi_trials_file = os.path.join(sitw_folder, 'eval', 'keys', 'core-multi.lst')
+    output_core_trials_file = os.path.join(output_folder, 'sitw_trials_core_core.txt')
+    output_multi_trials_file = os.path.join(output_folder, 'sitw_trials_core_multi.txt')
+    files = set()
+    enroll2utt = {}
+    with open(enroll_file) as f:
+        for line in f:
+            parts = line.split()
+            utt_id = parts[1].split('/')[1].strip()
+            files.add(utt_id)
+            enroll2utt[parts[0]] = utt_id
+    for filenames in [(core_trials_file, output_core_trials_file), (multi_trials_file, output_multi_trials_file)]:
+        with open(filenames[0]) as f, open(filenames[1], 'w') as outf:
+            for line in f:
+                parts = line.split()
+                utt_id = parts[1].split('/')[1]
+                files.add(utt_id)
+                label = parts[2].strip()
+                label = 'nontarget' if label == 'imp' else 'target'                   
+                outf.write('{} {} {}\n'.format(enroll2utt[parts[0]], utt_id, label))
+    wav_output = os.path.join(output_folder, 'wav.scp')
+    utt2spk_output = os.path.join(output_folder, 'utt2spk')
+    with open(wav_output, 'w') as wav_out, open(utt2spk_output, 'w') as utt2spk_out:
+        for filename in files:
+            filepath = os.path.join(sitw_folder, 'eval', 'audio', filename)
+            wav_out.write('{} ffmpeg -y -v 8 -i {} -ac 1 -ar {} -f wav - |\n'.format(filename, filepath, Settings().features.sampling_rate))
+            utt2spk_out.write('{0} {0}\n'.format(filename)) 
+    finalize(output_folder)
+    print('SITW prepared!')
+
+def _travel_voxceleb_folders(data_folders):
+    file_dict = {}
+    for data_folder in data_folders:
+        spk_folders = os.listdir(data_folder)
+        for spk in spk_folders:
+            spk_folder = os.path.join(data_folder, spk)
+            video_folders = os.listdir(spk_folder)
+            for video in video_folders:
+                video_folder = os.path.join(spk_folder, video)
+                files = os.listdir(video_folder)
+                files = [os.path.join(video_folder, filename) for filename in files]
+                file_dict[(spk, video)] = files
+    return file_dict
+
+def _prepare_voxceleb(data_folder, output_folder, output_folder_cat, wav_func, cat_wav_func):
+    file_dict = _travel_voxceleb_folders(data_folder)
+    ensure_exists(output_folder)
+    wav_output = os.path.join(output_folder, 'wav.scp')
+    utt2spk_output = os.path.join(output_folder, 'utt2spk')
+    ensure_exists(os.path.join(output_folder_cat, 'cat'))
+    wav_output_cat = os.path.join(output_folder_cat, 'wav.scp')
+    utt2spk_output_cat = os.path.join(output_folder_cat, 'utt2spk')
+    with open(wav_output, 'w') as wav_out, open(utt2spk_output, 'w') as utt2spk_out, open(wav_output_cat, 'w') as wav_out_cat, open(utt2spk_output_cat, 'w') as utt2spk_out_cat:
+        for key in file_dict:
+            cat_file = os.path.join(output_folder_cat, 'cat', '{}-{}.txt'.format(key[0], key[1]))
+            with open(cat_file, 'w') as cat_out:
+                for filename in file_dict[key]:
+                    utt_id = filename.split('/')[-1].split('.')[0]
+                    utt_id = '-'.join((key[0], key[1], utt_id))
+                    wav_out.write('{} {}\n'.format(utt_id, wav_func(filename)))
+                    utt2spk_out.write('{} {}\n'.format(utt_id, key[0]))
+                    cat_out.write("file '{}'\n".format(filename))
+            utt_id = '-'.join((key[0], key[1]))
+            wav_out_cat.write('{} {}\n'.format(utt_id, cat_wav_func(key)))
+            utt2spk_out_cat.write('{} {}\n'.format(utt_id, key[0]))
+    finalize(output_folder)
+    finalize(output_folder_cat)
+    
+def prepare_voxceleb1():
+    print('Preparing VoxCeleb1...')
+    data_folders = (os.path.join(Settings().paths.datasets['voxceleb1'], 'wav'), )
+    output_folder = fileutils.get_list_folder('voxceleb1')
+    output_folder_cat = fileutils.get_list_folder('voxceleb1_cat')
+    def wav_func(filename):
+        return filename
+    def cat_wav_func(key):
+        cat_file = os.path.join(output_folder_cat, 'cat', '{}-{}.txt'.format(key[0], key[1]))
+        return 'ffmpeg -f concat -safe 0 -i {} -c copy -f wav - |'.format(cat_file)
+    _prepare_voxceleb(data_folders, output_folder, output_folder_cat, wav_func, cat_wav_func)
+    print('VoxCeleb1 prepared!')
+
+def prepare_voxceleb2():
+    print('Preparing VoxCeleb2...')
+    output_folder = fileutils.get_list_folder('voxceleb2')
+    output_folder_cat = fileutils.get_list_folder('voxceleb2_cat')
+    vox2_folder = Settings().paths.datasets['voxceleb2']
+    data_folders = (os.path.join(vox2_folder, 'dev', 'aac'), os.path.join(vox2_folder, 'test', 'aac'))
+    def wav_func(filename):
+        return 'ffmpeg -y -v 8 -i {} -ac 1 -ar {} -f wav - |'.format(filename, Settings().features.sampling_rate)
+    def cat_wav_func(key):
+        cat_file = os.path.join(output_folder_cat, 'cat', '{}-{}.txt'.format(key[0], key[1]))
+        return 'ffmpeg -f concat -safe 0 -i {} -c copy -f adts pipe: | ffmpeg -y -v 8 -i pipe: -ac 1 -ar {} -f wav - |'.format(cat_file, Settings().features.sampling_rate)
+    _prepare_voxceleb(data_folders, output_folder, output_folder_cat, wav_func, cat_wav_func)
+    print('VoxCeleb2 prepared!')
+
+def finalize(output_folder):
+    subprocess.run(['utils/utt2spk_to_spk2utt.pl', os.path.join(output_folder, 'utt2spk')], stdout=open(os.path.join(output_folder, 'spk2utt'), 'w'), stderr=subprocess.STDOUT, cwd=Settings().paths.kaldi_recipe_folder)
+    result = subprocess.run(['utils/fix_data_dir.sh', output_folder], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=Settings().paths.kaldi_recipe_folder)
+    print(result.stdout.decode('utf-8'))
---
+++ asvtorch/recipes/sitw/xvector/configs/run_configs.py
@@ -0,0 +1,145 @@
+# pylint: skip-file
+
+# Prepares all datasets
+prepare
+recipe.start_stage = 0
+recipe.end_stage = 0
+
+# Prepare vox1
+prepare_vox1 < prepare
+recipe.preparation_datasets = ['voxceleb1']
+
+# Prepare vox2
+prepare_vox2 < prepare
+recipe.preparation_datasets = ['voxceleb2']
+
+# Prepare vox2
+prepare_sitw < prepare
+recipe.preparation_datasets = ['sitw']
+
+
+
+# Extracts all features (we use "cat" version of voxceleb2 for training)
+fe
+recipe.start_stage = 1
+recipe.end_stage = 1
+recipe.feature_extraction_datasets = ['voxceleb1_cat', 'voxceleb2_cat', 'sitw']
+
+# Extracts "non-cat" VoxCeleb features (not needed in the recipe)
+fe_vox2 < fe
+recipe.feature_extraction_datasets = ['voxceleb1', 'voxceleb2']
+
+
+
+# Augments the training data (5x) --- 5x is maximum amount supported
+aug
+recipe.start_stage = 2
+recipe.end_stage = 2
+recipe.augmentation_datasets = {'voxceleb1_cat': 5, 'voxceleb2_cat': 5}
+
+
+
+# Trains the default network architecture
+net
+paths.system_folder = 'full_system_default'
+recipe.start_stage = 5
+recipe.end_stage = 5
+network.min_clip_size = 200
+network.max_clip_size = 200
+network.print_interval = 500
+network.weight_decay = 0.001
+network.utts_per_speaker_in_epoch = 300
+network.eer_stop_epochs = 5
+network.max_epochs = 1000
+network.initial_learning_rate = 0.2
+network.min_loss_change_ratio = 0.01
+network.target_loss = 1.0
+network.epochs_per_train_call = 5
+network.max_batch_size_in_frames = 15000
+network.max_consecutive_lr_updates = 2
+
+# To resume training from a specific epoch:
+net_resume < net
+network.resume_epoch = 23
+
+# To train network with squeeze-and-excitation:
+net_se < net
+paths.system_folder = 'full_system_se'
+network.network_class = 'asvtorch.src.networks.architectures.StandardSeNet'
+
+# To train network with squeeze-and-excitation and residual blocks:
+net_resse < net
+paths.system_folder = 'full_system_resse'
+network.network_class = 'asvtorch.src.networks.architectures.StandardResSeNet'
+
+# To train network with LDE layer:
+net_lde_isotropic < net
+paths.system_folder = 'full_system_lde_isotropic'
+network.pooling_layer_type = 'clustering'
+network.cluster_assignment_mode = 'lde'
+network.supervector_mode = 'lde'
+network.normalize_supervector = False
+network.n_clusters = 64
+network.n_ghost_clusters = 0
+network.stat_size = 128
+
+# Another variant of LDE:
+net_lde_tied_diagonal < net_lde_isotropic
+paths.system_folder = 'full_system_lde_tied_diagonal'
+network.lde_covariance_type = 'shared_diagonal'
+
+# Network with netVLAD:
+net_netvlad < net_lde_isotropic
+paths.system_folder = 'full_system_netvlad'
+network.cluster_assignment_mode = 'net_vlad'
+network.supervector_mode = 'net_vlad'
+network.normalize_supervector = True
+
+
+
+# EMBEDDING EXTRACTION
+
+# Not runnable as is (just to let other configs to inherit it)
+emb
+recipe.start_stage = 7
+recipe.end_stage = 7
+recipe.selected_epoch = None  # None = select the last epoch automatically
+network.max_batch_size_in_frames = 30000
+
+# Below are the embedding extraction configs for different networks
+
+emb_net < emb < net
+
+emb_net_se < emb < net_se
+
+emb_net_resse < emb < net_resse
+
+emb_net_lde_isotropic < emb < net_lde_isotropic
+
+emb_net_lde_tied_diagonal < emb < net_lde_tied_diagonal
+
+emb_net_netvlad < emb < net_netvlad
+
+
+# SCORING
+
+# Not runnable as is (just to let other configs to inherit it)
+score
+recipe.start_stage = 9
+recipe.end_stage = 9
+backend.plda_dim = 200
+backend.plda_rcond = 1e-7
+backend.score_norm_full_cohort_size = 2000
+backend.score_norm_adaptive_cohort_size = 200
+
+score_net < score < emb_net
+
+score_net_se < score < emb_net_se
+
+score_net_resse < score < emb_net_resse
+
+score_net_lde_isotropic < score < emb_net_lde_isotropic
+
+score_net_lde_tied_diagonal < score < emb_net_lde_tied_diagonal
+
+score_net_netvlad < score < emb_net_netvlad
